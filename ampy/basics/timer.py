c8e5098c6e3c411e3aef87a9fbcf2cbe06850084
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-09 13:33:50 -0300

    timer.PERIODIC successfull

--- ampy/basics/timer.py
+++ ampy/basics/timer.py
@@ -8,16 +8,18 @@ yellowLed2 = Pin( 12, Pin.OUT)
 
 #Variables for timer
 led = 1 
-period = 1000
+period = 500
 #to set timer
 tim1 = Timer(1)
 tim1.init(period=period, mode=Timer.PERIODIC, callback=lambda t:
           setLed()
+          
 )
 
 #function for to set Led
 def setLed():
-    global led 
+    global led
+    print('Led On: {}',led) 
     if led == 1:
         blueLed.value(1)
         yellowLed1.off()
@@ -35,7 +37,7 @@ def setLed():
         blueLed.value(0)
         yellowLed1.off()
         yellowLed2.off()
-    print(f'Led On: {led}')
+    
     led +=1
     if led > 4:
         led =1
