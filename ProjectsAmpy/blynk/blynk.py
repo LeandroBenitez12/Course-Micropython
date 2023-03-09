29db8f905f5b574f3370b88bf1e64a901f3c7b88
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-09 12:47:17 -0300

    import time , add delays and Declared pinouts

--- ProjectsAmpy/blynk/blynk.py
+++ ProjectsAmpy/blynk/blynk.py
@@ -1,6 +1,12 @@
 from machine import Pin
+import time
 
 blueLed = Pin( 2, Pin.OUT)
+id = 0
+while id<4:
+    blueLed.on()
+    time.sleep(1.0)  # Delay for 1 second.
 
-blueLed.on()
-#blueLed.value(0)
+    blueLed.value(0)
+    time.sleep(1.0)  # Delay for 1 second.
+    id= id +1
