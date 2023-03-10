16d23394f457a05b456f8326d243f31bd32f250a
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-10 12:20:59 -0300

    changed file pwm configure the pwm

--- ampy/basics/pwm.py
+++ ampy/basics/pwm.py
@@ -13,22 +13,25 @@ dutyOff= 0
 led = 1
 period= 20
 
-#declared pinouts
-led_blue = PWM(Pin(PIN_BLUE_LED))
-led_yellow = PWM(Pin(PIN_YELLOW_LED))
-led_red = PWM(Pin(PIN_RED_LED))
+# #declared pinouts
+# led_blue = PWM(Pin(PIN_BLUE_LED))
+# led_yellow = PWM(Pin(PIN_YELLOW_LED))
+# led_red = PWM(Pin(PIN_RED_LED))
 
-#I declare the PWM Frequency (from 1Hz to 40MHz)
-#freq = pwm0.freq()         # get current frequency (default 5kHz)
-led_blue.freq(frequency)
-led_red.freq(frequency)
-led_yellow.freq(frequency)
+# #I declare the PWM Frequency (from 1Hz to 40MHz)
+# #freq = pwm0.freq()         # get current frequency (default 5kHz)
+# led_blue.freq(frequency)
+# led_red.freq(frequency)
+# led_yellow.freq(frequency)
+# # get current duty cycle, range 0-1023 (default 512, 50%)
+# led_blue.duty(dutyOff)
+# led_yellow.duty(dutyOff)
+# led_red.duty(dutyOff)       
 
-
-# get current duty cycle, range 0-1023 (default 512, 50%)
-led_blue.duty(dutyOff)
-led_yellow.duty(dutyOff)
-led_red.duty(dutyOff)       
+#Method to get all in one line
+led_blue = PWM(Pin(PIN_BLUE_LED), freq=frequency, duty=dutyOff)  # create and configure in one go
+led_yellow = PWM(Pin(PIN_YELLOW_LED), freq=frequency, duty=dutyOff)# dutyOff inicializate off
+led_red = PWM(Pin(PIN_RED_LED), freq=frequency, duty=dutyOff)
 
 #timer functions Periodics
 tim= Timer(1)
