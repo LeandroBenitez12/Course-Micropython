476de4a778ac2bdcbb06832e349353339c162e84
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-10 12:32:44 -0300

    file ADC in test

--- ampy/basics/ADC.py
+++ ampy/basics/ADC.py
@@ -1,13 +1,17 @@
 from machine import Pin
 from machine import ADC
+from machine import Timer
 
 PIN_CNY70 = 32
 PIN_YELLOW_LED = 14
 
 yellow_led = Pin(PIN_YELLOW_LED, Pin.OUT)
 
-cny70 = ADC(PIN_CNY70)
-
+cny70 = ADC(Pin(PIN_CNY70))
+cny70.atten(ADC.ATTN_11DB)
 read_cny70 = cny70.read()
-while True:
+
+tim1= Timer(1)
+tim1.init(period=500, mode= Timer.PERIODIC, callback = lambda t:
     print(read_cny70)
+)
