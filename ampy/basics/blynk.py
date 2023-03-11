1bbe41e8bfef7595ca9f32176ee3e0e47b6d5f66
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-11 00:00:56 -0300

    Tested in MU

--- ampy/basics/blynk.py
+++ ampy/basics/blynk.py
@@ -1,12 +1,13 @@
 from machine import Pin
 import time
 
-blueLed = Pin( 2, Pin.OUT)
+blueLed = Pin(2, Pin.OUT)
 id = 0
-while id<4:
+
+while id < 4:
     blueLed.on()
     time.sleep(1.0)  # Delay for 1 second.
 
     blueLed.value(0)
     time.sleep(1.0)  # Delay for 1 second.
-    id= id +1
+    id = id + 1
