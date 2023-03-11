4791d16a725fab819a76fc0d8b87ba9b5104b728
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-11 16:09:29 -0300

    getting started with the wifi in micropython
    i am doing the scan of wifi

---
+++ ampy/basics/wifi.py
@@ -0,0 +1,16 @@
+from machine import Pin 
+import network  
+PIN_LED_BLUE = 2
+PIN_LED_YELLOW = 14
+
+led_blue =Pin(PIN_LED_BLUE , Pin.OUT)
+led_yellow =Pin(PIN_LED_YELLOW , Pin.OUT)
+
+led_blue.off()
+led_yellow.on()
+
+wlan = network.WLAN(network.STA_IF) # create station interface
+wlan.active(True)       # activate the interface
+wifi_list = wlan.scan()        # scan for access points
+
+print(wifi_list)
