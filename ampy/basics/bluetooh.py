6f33ef586cf22805465c4b3e81c1ca88e07d50ba
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-13 23:15:35 -0300

    it still does not work

--- ampy/basics/bluetooh.py
+++ ampy/basics/bluetooh.py
@@ -1,4 +1,6 @@
 from machine import Pin
+import bluetooth
+from BLE import BLEUART
 
 PIN_LED_BLUE = 2
 PIN_LED_YELLOW = 14
@@ -6,3 +8,22 @@ PIN_LED_YELLOW = 14
 led_blue =Pin(PIN_LED_BLUE , Pin.OUT)
 led_yellow =Pin(PIN_LED_YELLOW , Pin.OUT)
 
+led_yellow.off()
+led_blue.off()
+# INIT BLUETOOTH 
+name = "ESP32"
+ble = bluetooth.BLE()
+uart = BLEUART(ble, name, rxbuf=1000)
+
+# bluetooh rx 
+rx = uart.read().decode().strip()
+uart.write('Esp32 says: '+ str(rx)+ '\n')
+if rx == 'B':
+    led_blue.on()
+    
+elif rx == 'Y':
+    led_yellow.on()
+    led_blue.off()
+elif rx == 'A':
+    led_yellow.on()
+    led_blue.on()
